6e54f4cb712242d95d90384455108882df50ca60
leechee <papa_100@163.com>
2017-10-10 21:48:48 +0800

    ejs语法

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# node-ejs
-node.js ejs学习
+## 项目说明
+- 该工程是一个简单的 node.js web 项目，使用 koa2 框架和 ejs 模板渲染！
+- 通过该项目中的实例代码，让大家更快速的学习 ejs 的语法，以及更好的使用ejs模板！
+- 该工程 ejs 模板的代码都在 views/ 文件夹下
+
+## 项目启动
+1. npm install
+2. npm start
+3. 访问 localhost:3000 即可看到 ejs 模板的渲染效果
+
+## ejs 常用语法
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,17 @@
 const router = require('koa-router')()
 
-router.get('/', async (ctx, next) => {
-  await ctx.render('index', {
-    title: 'Hello Koa 2!'
-  })
-})
-
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
-})
-
-router.get('/json', async (ctx, next) => {
-  ctx.body = {
-    title: 'koa2 json'
+router.get('/', async (ctx) => {
+  const data = {
+    title: '陈奕迅个人档案',
+    user: {
+      name: '陈奕迅',
+      age: 15,
+      sex: '男',
+      html: '<a href="https://baike.baidu.com/item/%E9%99%88%E5%A5%95%E8%BF%85/128029?fr=aladdin">陈奕迅-百度百科</a>'
+    },
+    tags: ['歌手', '明星', '偶像']
   }
+  await ctx.render('index', data)
 })
 
 module.exports = router
